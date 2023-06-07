8c9525b32a6da58860c1f4e2120a19a9e1a16f44
shakilwebmaster <mdshakil102036@gmail.com>
2023-06-07 22:14:22 +0600

    authentication add

---
+++ helpers/auth.js
@@ -0,0 +1,8 @@
+const jwt = require('jsonwebtoken');
+
+const generateToken = (userId, secretKey)  =>{
+    const payload = {userId};
+    const options = {expireIn: '1h'}
+
+    return jwt.sign(payload, secretKey, options);
+}
